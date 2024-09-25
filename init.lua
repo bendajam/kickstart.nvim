f6439f54bd5fa49e702d66e0f8f504da81280e3e
bmetzger <bmetzger@leepfrog.com>
2024-09-25 14:14:15 -0500

    add in defaults to what lsps are being installed

--- init.lua
+++ init.lua
@@ -607,6 +607,10 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ts_ls',
+        'eslint',
+        'jsonls',
+        'eslint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
