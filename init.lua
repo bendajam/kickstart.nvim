3a671b70d949a7abdbc471c92270e6964747bdc6
bmetzger <bmetzger@leepfrog.com>
2024-10-14 16:02:29 -0500

    Add in gv for grep visual selection

--- init.lua
+++ init.lua
@@ -385,6 +385,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>gw', live_grep_args_shortcuts.grep_word_under_cursor)
+      vim.keymap.set('v', '<leader>gv', live_grep_args_shortcuts.grep_visual_selection)
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
